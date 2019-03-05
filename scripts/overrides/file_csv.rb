3e2b62a7ecc7c222d440a5f9eb46076ba24b18b3
Jessica Dussault <jdussault4@gmail.com>
2019-03-05 12:42:17 -0600

    small changes to csv data and temp fix for solr ruby script
    csv looks as though everything has been changed which is due to line ending
    alteration done with my text editor

--- scripts/overrides/file_csv.rb
+++ scripts/overrides/file_csv.rb
@@ -17,4 +17,29 @@ class FileCsv
     doc.add_child("<field name='collection'>#{row['collection']}</field>") if row['collection']
     return doc
   end
+
+  # TODO this is a temporary fix because datura is returning "docs" instead of "doc"
+  # but once datura is fixed and this repo is bumped, transform_solr override here can be removed
+  def transform_solr
+    puts "transforming #{self.filename}"
+    solr_doc = Nokogiri::XML("<add></add>")
+    @csv.each do |row|
+      if !row.header_row?
+        doc = Nokogiri::XML::Node.new("doc", solr_doc)
+        # row_to_solr should return an XML::Node object with children
+        doc = row_to_solr(doc, @csv.headers, row)
+        solr_doc.at_css("add").add_child(doc)
+      end
+    end
+    # Uncomment to debug
+    # puts solr_doc.root.to_xml
+    if @options["output"]
+      filepath = "#{@out_solr}/#{self.filename(false)}.xml"
+      # puts "output #{@out_solr}"
+      File.open(filepath, "w") { |f| f.write(solr_doc.root.to_xml) }
+    end
+    return { "doc" => solr_doc.root.to_xml }
+  end
+
+
 end
